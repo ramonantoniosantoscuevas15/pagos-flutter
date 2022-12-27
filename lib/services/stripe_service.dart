5ff3720daf684c5dfea2c43f4ef935caad3e6f52
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2022-12-27 10:32:02 -0400

    cambios

--- lib/services/stripe_service.dart
+++ lib/services/stripe_service.dart
@@ -7,10 +7,10 @@ class StripeService {
   StripeService._privateConstrotor();
   static final StripeService _intance = StripeService._privateConstrotor();
   factory StripeService() => _intance;
-  String _paymentApiUrl = 'https://api.stripe.com/v1/payment_intents';
+   String _paymentApiUrl = 'https://api.stripe.com/v1/payment_intents';
   static String _secretKey =
       'sk_test_51MJJ0dDzWSzd4c55nLpnkzvPh7Rr1aInUzNjCHfgGgGXPH1cDIkHYGa8mEWnTEdz7jpnNRtNFO9vijVWtDvyy6t600JgZoPoyj';
-  String _apiKey =
+   String _apiKey =
       'pk_test_51MJJ0dDzWSzd4c550foUaHz2GBAM1wFeXCQkfb9LXIME75hnZkOohJ3jWQ3ZtJfd6uBICESirQQWS2QOCyWuzprs00TmNaDyV9';
   final headerOptions =
       Options(contentType: Headers.formUrlEncodedContentType, headers: {
@@ -36,9 +36,10 @@ class StripeService {
     try {
       final paymentMethod = await StripePayment.paymentRequestWithCardForm(
           CardFormPaymentRequest());
-      //crear el intent
-      final resp = await _crearPaymentIntent(amount: amount, currency: currency);
-      return StripeCustomResponse(ok: true);
+      final resp = await _realizarPago(
+          amount: amount, currency: currency, paymentMethod: paymentMethod);
+
+      return resp;
     } catch (e) {
       return StripeCustomResponse(ok: false, msg: e.toString());
     }
@@ -62,14 +63,37 @@ class StripeService {
           await dio.post(_paymentApiUrl, data: data, options: headerOptions);
       return PaymentIntentResponse.fromJson(resp.data);
     } catch (e) {
+      // ignore: avoid_print
       print('Error en intento: ${e.toString()}');
       return PaymentIntentResponse(status: '400');
     }
   }
 
-  Future _realizarPago({
+  Future<StripeCustomResponse> _realizarPago({
     required String amount,
     required String currency,
     required PaymentMethod paymentMethod,
-  }) async {}
+  }) async {
+    try {
+      //crear el intent
+      final paymentIntent =
+          await _crearPaymentIntent(amount: amount, currency: currency);
+      final paymentResult = await StripePayment.confirmPaymentIntent(
+          PaymentIntent(
+              clientSecret: paymentIntent.clientSecret,
+              paymentMethodId: paymentMethod.id));
+      if (paymentResult.status == 'succeeded') {
+        return StripeCustomResponse(ok: true);
+      }else{
+        return StripeCustomResponse(
+          ok: false,
+          msg: 'Fallo: ${paymentResult.status}'
+          );
+      }
+    } catch (e) {
+      // ignore: avoid_print
+      print(e.toString());
+      return StripeCustomResponse(ok: false, msg: e.toString());
+    }
+  }
 }
