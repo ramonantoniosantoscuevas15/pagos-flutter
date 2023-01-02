b31a40d8737705fd74b7ca6fa2c2e04a232f5497
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2023-01-02 10:07:06 -0400

    cambios

--- lib/services/stripe_service.dart
+++ lib/services/stripe_service.dart
@@ -7,10 +7,11 @@ class StripeService {
   StripeService._privateConstrotor();
   static final StripeService _intance = StripeService._privateConstrotor();
   factory StripeService() => _intance;
-   String _paymentApiUrl = 'https://api.stripe.com/v1/payment_intents';
-  static String _secretKey =
+   final String _paymentApiUrl = 'https://api.stripe.com/v1/payment_intents';
+   // ignore: prefer_final_fields
+   static String _secretKey =
       'sk_test_51MJJ0dDzWSzd4c55nLpnkzvPh7Rr1aInUzNjCHfgGgGXPH1cDIkHYGa8mEWnTEdz7jpnNRtNFO9vijVWtDvyy6t600JgZoPoyj';
-   String _apiKey =
+   final String _apiKey =
       'pk_test_51MJJ0dDzWSzd4c550foUaHz2GBAM1wFeXCQkfb9LXIME75hnZkOohJ3jWQ3ZtJfd6uBICESirQQWS2QOCyWuzprs00TmNaDyV9';
   final headerOptions =
       Options(contentType: Headers.formUrlEncodedContentType, headers: {
@@ -52,6 +53,7 @@ class StripeService {
   Future<PaymentIntentResponse> _crearPaymentIntent({
     required String amount,
     required String currency,
+    
   }) async {
     try {
       final dio = Dio();
@@ -65,7 +67,7 @@ class StripeService {
     } catch (e) {
       // ignore: avoid_print
       print('Error en intento: ${e.toString()}');
-      return PaymentIntentResponse(status: '400');
+      return PaymentIntentResponse(status: '400',);
     }
   }
 
@@ -77,7 +79,7 @@ class StripeService {
     try {
       //crear el intent
       final paymentIntent =
-          await _crearPaymentIntent(amount: amount, currency: currency);
+          await _crearPaymentIntent(amount: amount, currency: currency,);
       final paymentResult = await StripePayment.confirmPaymentIntent(
           PaymentIntent(
               clientSecret: paymentIntent.clientSecret,
